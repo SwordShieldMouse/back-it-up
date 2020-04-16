fixed a bunch of plot scripts (including bug in sensitivity plot script). Added hard_policy forward/reverse kl (softmaxtemp=0). Added normalized bandit environment.

deep_experiments/agents/network/forwardkl_network.py
@@ -34,6 +34,8 @@ class ForwardKLNetwork(BaseNetwork):
         else:
             self.batch_size = 1
 
+        self.use_hard_policy = config.use_hard_policy
+
         # create network
         if config.env_name == 'ContinuousBandits':
             self.pi_net = LinearPolicyNetwork(self.state_dim, self.action_dim, self.action_max[0])
@@ -154,39 +156,54 @@ class ForwardKLNetwork(BaseNetwork):
             value_loss = nn.MSELoss()(v_val, target_v_val.detach())
 
         # pi_loss
-        if self.optim_type == 'll':
-            raise NotImplementedError
+        if not self.use_hard_policy:
+            if self.optim_type == 'll':
+                raise NotImplementedError
 
-        elif self.optim_type == 'intg':
-            tiled_state_batch = state_batch.unsqueeze(1).repeat(1, self.intgrl_actions_len, 1)
-            stacked_state_batch = tiled_state_batch.reshape(-1, self.state_dim)
+            elif self.optim_type == 'intg':
+                tiled_state_batch = state_batch.unsqueeze(1).repeat(1, self.intgrl_actions_len, 1)
+                stacked_state_batch = tiled_state_batch.reshape(-1, self.state_dim)
 
-            if self.use_true_q:
-                # predict_true_q
-                intgrl_q_val = torch.from_numpy(self.predict_true_q(stacked_state_batch, self.stacked_intgrl_actions)).to(torch.float32)
+                if self.use_true_q:
+                    # predict_true_q
+                    intgrl_q_val = torch.from_numpy(self.predict_true_q(stacked_state_batch, self.stacked_intgrl_actions)).to(torch.float32)
+                else:
+                    intgrl_q_val = self.q_net(stacked_state_batch, self.stacked_intgrl_actions)
+
+
+                tiled_intgrl_q_val = intgrl_q_val.reshape(-1, self.intgrl_actions_len) / self.entropy_scale
+
+                # compute Z
+                constant_shift, _ = torch.max(tiled_intgrl_q_val, -1, keepdim=True)
+                tiled_constant_shift = constant_shift.repeat(1, self.intgrl_actions_len)
+
+                intgrl_exp_q_val = torch.exp(tiled_intgrl_q_val - tiled_constant_shift).detach()
+
+                z = (intgrl_exp_q_val * self.tiled_intgrl_weights).sum(-1).detach()
+                tiled_z = z.unsqueeze(-1).repeat(1, self.intgrl_actions_len).detach()
+
+                boltzmann_prob = intgrl_exp_q_val / tiled_z
+
+                intgrl_logprob = self.pi_net.get_logprob(state_batch, self.tiled_intgrl_actions)
+                tiled_intgrl_logprob = intgrl_logprob.reshape(self.batch_size, self.intgrl_actions_len)
+
+                integrands = boltzmann_prob * tiled_intgrl_logprob
+                policy_loss = (-(integrands * self.tiled_intgrl_weights).sum(-1)).mean(-1)
+
             else:
-                intgrl_q_val = self.q_net(stacked_state_batch, self.stacked_intgrl_actions)
-            tiled_intgrl_q_val = intgrl_q_val.reshape(-1, self.intgrl_actions_len) / self.entropy_scale
-
-            # compute Z
-            constant_shift, _ = torch.max(tiled_intgrl_q_val, -1, keepdim=True)
-            tiled_constant_shift = constant_shift.repeat(1, self.intgrl_actions_len)
-
-            intgrl_exp_q_val = torch.exp(tiled_intgrl_q_val - tiled_constant_shift).detach()
-
-            z = (intgrl_exp_q_val * self.tiled_intgrl_weights).sum(-1).detach()
-            tiled_z = z.unsqueeze(-1).repeat(1, self.intgrl_actions_len).detach()
-
-            boltzmann_prob = intgrl_exp_q_val / tiled_z
-
-            intgrl_logprob = self.pi_net.get_logprob(state_batch, self.tiled_intgrl_actions)
-            tiled_intgrl_logprob = intgrl_logprob.reshape(self.batch_size, self.intgrl_actions_len)
-
-            integrands = boltzmann_prob * tiled_intgrl_logprob
-            policy_loss = (-(integrands * self.tiled_intgrl_weights).sum(-1)).mean(-1)
-
+                raise ValueError("Invalid optim_type")
         else:
-            raise ValueError("Invalid optim_type")
+            if self.use_true_q:
+
+                # 1x1x1
+                dummy_state_batch = torch.FloatTensor([0]).to(self.device).unsqueeze(-1).unsqueeze(-1)
+                dummy_action_batch = torch.FloatTensor([getattr(environments.environments, self.config.env_name).get_max()]).to(self.device).unsqueeze(-1).unsqueeze(-1)
+
+                print()
+                policy_loss = (-self.pi_net.get_logprob(dummy_state_batch, dummy_action_batch)).mean()
+
+            else:
+                raise ValueError("Need to find explicit maximum, and need trueQ")
 
         if not self.use_true_q:
             self.q_optimizer.zero_grad()

deep_experiments/agents/network/reversekl_network.py
@@ -34,6 +34,8 @@ class ReverseKLNetwork(BaseNetwork):
 
         self.use_target = config.use_target
 
+        self.use_hard_policy = config.use_hard_policy
+
         # create network
         if config.env_name == 'ContinuousBandits':
             self.pi_net = LinearPolicyNetwork(self.state_dim, self.action_dim, self.action_max[0])
@@ -157,35 +159,61 @@ class ReverseKLNetwork(BaseNetwork):
             value_loss = nn.MSELoss()(v_val, target_v_val.detach())
 
         # pi_loss
-        if self.optim_type == 'll':
-            log_prob_target = new_q_val - v_val
 
-            # loglikelihood update
-            policy_loss = (-log_prob * (log_prob_target - self.entropy_scale * log_prob).detach()).mean()
+        if not self.use_hard_policy:
+            if self.optim_type == 'll':
+                log_prob_target = new_q_val - v_val
 
-        elif self.optim_type == 'hard_ll':
-            log_prob_target = new_q_val - v_val
-            policy_loss = (-log_prob * (log_prob_target).detach()).mean()
+                # loglikelihood update
+                policy_loss = (-log_prob * (log_prob_target - self.entropy_scale * log_prob).detach()).mean()
 
-        elif self.optim_type == 'intg':
-            stacked_state_batch = state_batch.unsqueeze(1).repeat(1, self.intgrl_actions_len, 1).reshape(-1, self.state_dim)
+            # TODO: probably remove this from the category
+            elif self.optim_type == 'hard_ll':
+                log_prob_target = new_q_val - v_val
+                policy_loss = (-log_prob * (log_prob_target).detach()).mean()
+
+            elif self.optim_type == 'intg':
+                stacked_state_batch = state_batch.unsqueeze(1).repeat(1, self.intgrl_actions_len, 1).reshape(-1, self.state_dim)
+
+                if self.use_true_q:
+                    intgrl_q_val = torch.from_numpy(self.predict_true_q(stacked_state_batch, self.stacked_intgrl_actions)).to(torch.float32)
+
+                    intgrl_logprob = self.pi_net.get_logprob(state_batch, self.tiled_intgrl_actions)
+
+                    integrands = - torch.exp(intgrl_logprob.squeeze()) * (
+                                (intgrl_q_val.squeeze()).detach() - self.entropy_scale * intgrl_logprob.squeeze())
+                else:
+                    intgrl_q_val = self.q_net(stacked_state_batch, self.stacked_intgrl_actions)
+                    intgrl_v_val = v_val.unsqueeze(1).repeat(1, self.intgrl_actions_len, 1).reshape(-1, 1)
+
+                    intgrl_logprob = self.pi_net.get_logprob(state_batch, self.tiled_intgrl_actions)
+
+                    integrands = - torch.exp(intgrl_logprob.squeeze()) * ((intgrl_q_val.squeeze() - intgrl_v_val.squeeze()).detach() - self.entropy_scale * intgrl_logprob.squeeze())
+
+                policy_loss = (integrands * self.intgrl_weights.repeat(self.batch_size)).reshape(self.batch_size, -1).sum(-1).mean(-1)
+
+        else:
+            stacked_state_batch = state_batch.unsqueeze(1).repeat(1, self.intgrl_actions_len, 1).reshape(-1,self.state_dim)
 
             if self.use_true_q:
-                intgrl_q_val = torch.from_numpy(self.predict_true_q(stacked_state_batch, self.stacked_intgrl_actions)).to(torch.float32)
+                intgrl_q_val = torch.from_numpy(
+                    self.predict_true_q(stacked_state_batch, self.stacked_intgrl_actions)).to(torch.float32)
 
                 intgrl_logprob = self.pi_net.get_logprob(state_batch, self.tiled_intgrl_actions)
 
-                integrands = - torch.exp(intgrl_logprob.squeeze()) * (
-                            (intgrl_q_val.squeeze()).detach() - self.entropy_scale * intgrl_logprob.squeeze())
+                integrands = - torch.exp(intgrl_logprob.squeeze()) * intgrl_q_val.squeeze()  # .detach())
             else:
-                intgrl_q_val = self.q_net(stacked_state_batch, self.stacked_intgrl_actions)
-                intgrl_v_val = v_val.unsqueeze(1).repeat(1, self.intgrl_actions_len, 1).reshape(-1, 1)
+                raise NotImplementedError()
+                # intgrl_q_val = self.q_net(stacked_state_batch, self.stacked_intgrl_actions)
+                # intgrl_v_val = v_val.unsqueeze(1).repeat(1, self.intgrl_actions_len, 1).reshape(-1, 1)
+                #
+                # intgrl_logprob = self.pi_net.get_logprob(state_batch, self.tiled_intgrl_actions)
+                #
+                # integrands = - torch.exp(intgrl_logprob.squeeze()) * ((
+                #                                                                   intgrl_q_val.squeeze() - intgrl_v_val.squeeze()).detach() - self.entropy_scale * intgrl_logprob.squeeze())
 
-                intgrl_logprob = self.pi_net.get_logprob(state_batch, self.tiled_intgrl_actions)
-
-                integrands = - torch.exp(intgrl_logprob.squeeze()) * ((intgrl_q_val.squeeze() - intgrl_v_val.squeeze()).detach() - self.entropy_scale * intgrl_logprob.squeeze())
-
-            policy_loss = (integrands * self.intgrl_weights.repeat(self.batch_size)).reshape(self.batch_size, -1).sum(-1).mean(-1)
+            policy_loss = (integrands * self.intgrl_weights.repeat(self.batch_size)).reshape(self.batch_size, -1).sum(
+                -1).mean(-1)
 
         if not self.use_true_q:
             self.q_optimizer.zero_grad()

deep_experiments/plot_scripts/find_agent_best_setting.py
@@ -23,7 +23,6 @@ import os
 # Use if you want to plot specific settings, put the idx of the setting below.
 # You can also see *_Params.txt to see the idx for each setting.
 
-parse_type = 'entropy_scale'
 show_plot = False
 
 plot_each_runs = True
@@ -50,8 +49,8 @@ def get_xyrange(envname):
         ymax = [0, 0]
 
     elif envname == "Swimmer-v2":
-        ymin = [20, 20]
-        ymax = [120, 120]
+        ymin = [0, 0]
+        ymax = [50, 50]
 
     else:
         raise ValueError("Invalid environment name")
@@ -64,8 +63,8 @@ def get_xyrange(envname):
 
 if __name__ == "__main__":
 
-    if len(sys.argv) != 7:
-        raise ValueError('Invalid input. \nCorrect Usage: find_agent_best_setting.py merged_result_loc, root_dir, env_name, agent_name, num_runs custom_save_name')
+    if len(sys.argv) != 8:
+        raise ValueError('Invalid input. \nCorrect Usage: find_agent_best_setting.py merged_result_loc, root_dir, env_name, agent_name, num_runs custom_save_name parse_type')
 
 
     root_dir = str(sys.argv[2])
@@ -79,6 +78,7 @@ if __name__ == "__main__":
     num_runs = int(sys.argv[5])
 
     custom_save_name = str(sys.argv[6])
+    parse_type = str(sys.argv[7])
 
     with open(env_json_dir, 'r') as env_dat:
         env_json = json.load(env_dat, object_pairs_hook=OrderedDict)
@@ -121,14 +121,14 @@ if __name__ == "__main__":
     EVAL_EPISODES = env_json['EvalEpisodes']
 
      # Plot type
-    result_type = ['EvalEpisode']
+    result_type = ['TrainEpisode', 'EvalEpisode']
 
     title = "%s, %s: %s (%d runs)" % (env_name, agent_name, custom_save_name, num_runs)
 
-    plt.figure(figsize=(12, 6))
-    plt.title(title)
-
     for result_idx, result in enumerate(result_type):
+
+        plt.figure(figsize=(12, 6))
+        plt.title(title)
 
         lcfilename = merged_result_dir + env_name + '_' + agent_name + '_' + result + 'MeanRewardsLC.txt'
         print('Reading lcfilename.. ' + lcfilename)
@@ -144,6 +144,7 @@ if __name__ == "__main__":
         files = glob.glob(paramfile)
         params = np.loadtxt(files[0], delimiter=',', dtype='str')
 
+        print("{} lc dim: {} x {}".format(result, len(lc), len(lc[0])))
         # default xmax
         xmax = np.shape(lc)[-1]
 
@@ -153,7 +154,7 @@ if __name__ == "__main__":
 
         last_N = int(last_N_ratio * xmax)
         if result == 'TrainEpisode':
-            raise NotImplementedError
+            plt.xlabel('Training Steps (per 1000 steps)')
 
         elif result == 'EvalEpisode':
             plt.xlabel('Training Steps (per 1000 steps)')
@@ -240,7 +241,7 @@ if __name__ == "__main__":
         if show_plot:
             plt.show()
         else:
-            plt.savefig("{}_{}_{}.png".format(env_name, agent_name, custom_save_name))
+            plt.savefig("{}_{}_{}.png".format(env_name, agent_name, result + '_' + custom_save_name))
         plt.close()
 
         savelc = bestlc
@@ -253,12 +254,12 @@ if __name__ == "__main__":
         np.save(savefilename_se, savelcse)
 
 
-    # just call plot_Bimodal.py
-    if plot_each_runs:
+        # just call plot_Bimodal.py
+        if plot_each_runs:
 
-        for i in range(num_type):
-            print("*** plotting each run for {}: {} --- {}".format(parse_type, type_arr[i], int(type_best_arr[i])))
-            os.system("python3 {}/plot_scripts/plot_Bimodal.py {}results {}/jsonfiles/environment/{}.json {} {} {} {}_{}_{}_runs".format(
-                root_dir, env_name, root_dir, env_name, num_runs, agent_name, int(type_best_arr[i]), custom_save_name, parse_type, type_arr[i]))
+            for i in range(num_type):
+                print("*** plotting each run for {}: {} --- {}".format(parse_type, type_arr[i], int(type_best_arr[i])))
+                os.system("python3 {}/plot_scripts/plot_Bimodal.py {}results {}/jsonfiles/environment/{}.json {} {} {} {}_{}_{} {}".format(
+                    root_dir, env_name, root_dir, env_name, num_runs, agent_name, int(type_best_arr[i]), custom_save_name, parse_type, type_arr[i], result))
 
 

deep_experiments/plot_scripts/plot_sensitivity.py
@@ -7,27 +7,30 @@ from pathlib import Path
 from collections import OrderedDict
 import json
 
-root_dir = str(sys.argv[1])
-store_dir = str(sys.argv[2])
+# list of agent.json names
+agents = ['ForwardKL', 'ReverseKL']
+params = ['actor_critic_dim', 'pi_lr', 'qf_vf_lr', 'entropy_scale', 'q_update_type']
+
+store_dir = str(sys.argv[1])
+env_name = str(sys.argv[2])
 
 eval_last_N = True
 last_N_ratio = 0.5
 
-policy_lr_curve_data = {}
-value_lr_curve_data = {}
-
-
 # Pendulum
-env_name = 'Swimmer-v2'
 # plt_yticks = [-1600, -1200, -800, -400, -200, 0]
 
-# for agent_filename in ['forward_kl_bandits', 'reverse_kl_bandits']:
-for agent_filename in ['forward_kl', 'reverse_kl']:
+dicts = {}
+for p in params:
+    dicts[p] = {}
 
-    sweep_params = ['pi_lr', 'qf_vf_lr', 'entropy_scale', 'batch_size']
+# for agent_filename in ['forward_kl_bandits', 'reverse_kl_bandits']:
+for agent_name in agents:
+
+    sweep_params = params
 
     # load json
-    json_dir = '{}/jsonfiles/agent/{}.json'.format(root_dir, agent_filename)
+    json_dir = '{}/merged{}results/{}_{}_agent_Params.json'.format(store_dir, env_name, env_name, agent_name)
 
     with open(json_dir, 'r') as agent_dat:
 
@@ -50,6 +53,7 @@ for agent_filename in ['forward_kl', 'reverse_kl']:
     x = 1
     y = 1
 
+    print("agent: {}".format(agent_name))
     for p in sweep_params:
         print("cur sweep param: {}".format(p))
 
@@ -71,7 +75,9 @@ for agent_filename in ['forward_kl', 'reverse_kl']:
 
             # find result and mean
             # param_sweep_array.append(idx_array)
-
+            # print(len(idx_array))
+            # if (len(idx_array)) == 108:
+            #     print(idx_array)
             result_array = []
             for idx in idx_array:
                 xmax = len(train_mean_result[idx])
@@ -79,7 +85,7 @@ for agent_filename in ['forward_kl', 'reverse_kl']:
                     last_N = int(xmax * last_N_ratio)
                 else:
                     last_N = 0
-                result_array.append(np.mean(train_mean_result[idx][xmax-last_N:]))
+                result_array.append(np.sum(train_mean_result[idx][xmax-last_N:]))
 
             plt_cum_reward_y.append(np.max(result_array))
         y *= cur_param_num
@@ -90,66 +96,62 @@ for agent_filename in ['forward_kl', 'reverse_kl']:
         plt_x = plt_x[::-1]
         # plt_xticks = plt_xticks[::-1]
         plt_cum_reward_y = plt_cum_reward_y[::-1]
-        plt.plot(plt_xticks, plt_cum_reward_y)
+        # plt.plot(plt_xticks, plt_cum_reward_y)
 
-        # if p in ['actor_lr', 'pi_lr']:
-        #     policy_lr_curve_data[agent_name] = (p, plt_xticks, plt_x, plt_cum_reward_y.copy())
-        # elif p in ['expert_lr', 'qnet_lr', 'learning_rate', 'critic_lr', 'qf_vf_lr']:
-        #     value_lr_curve_data[agent_name] = (p, plt_xticks, plt_x, plt_cum_reward_y.copy())
+        dicts[p][agent_name] = (p, plt_xticks, plt_x, plt_cum_reward_y.copy())
 
-        plt.title("{}: {} sensitivity curve".format(agent_name, p))
-        plt.xlabel("{}".format(p))
-        plt.ylabel("Avg. {} AUC ".format(last_N_ratio), rotation=90)
-        plt.xticks(plt_xticks, plt_x)
-        # plt.yticks(,)
-
-        # plt.show()
-        plt.savefig("{}_{}_sensitivity_curve.png".format(agent_name, p))
-        plt.clf()
+        # if you want to show sensitivity for each agent individually
+        # plt.title("{}: {} sensitivity curve".format(agent_name, p))
+        # plt.xlabel("{}".format(p))
+        # plt.ylabel("Avg. {} AUC ".format(last_N_ratio), rotation=90)
+        # plt.xticks(plt_xticks, plt_x)
+        #
+        # plt.savefig("{}_{}_sensitivity_curve.png".format(agent_name, p))
+        # plt.clf()
 
 # Combined plots
-#
-# print(value_lr_curve_data.keys())
-# exit()
+
+show_label = True
+
+colors = [ '#377eb8', '#4daf4a', '#ff7f00',
+                  '#f781bf', '#984ea3', '#999999','#a65628',
+                  '#e41a1c', '#999999', '#dede00']
 
 
-# show_label = False
-#
-# colors = [ '#377eb8', '#4daf4a', '#ff7f00',
-#                   '#f781bf', '#984ea3', '#999999','#a65628',
-#                   '#e41a1c', '#999999', '#dede00']
-#
-#
-# # q-learning methods
-# # ae, ae_plus, qt_opt, naf, picnn, wirefitting, sql, optimalq
-#
-# # Bimodal
-# # for idx, a in enumerate(['ActorExpert', 'ActorExpert_Plus', 'SoftQlearning', 'NAF', 'PICNN', 'QT_OPT', 'WireFitting', 'OptimalQ']):
-# # Pendulum
-# for idx, a in enumerate(['ActorExpert_Separate', 'ActorExpert_Plus_Separate', 'SoftQlearning', 'NAF', 'PICNN', 'QT_OPT']):
-#
-#     plt_xticks = value_lr_curve_data[a][1]
-#     plt_x = value_lr_curve_data[a][2]
-#     plt_y = value_lr_curve_data[a][3]
-#     plt.plot(plt_xticks, plt_y, colors[idx], label="{}: {}".format(a, value_lr_curve_data[a][0]))
-#     plt.ylim(0, 1.5)
-#
-#     if show_label:
-#         plt.xticks(plt_xticks, plt_x)
-#         plt.yticks(plt_yticks, plt_yticks)
-#
-#     else:
-#         plt.xticks(plt_xticks, [])
-#         plt.yticks(plt_yticks, [])
-#
-# if show_label:
-#     plt.legend()
-#     plt.title("Q-learning: Value LR sensitivity curve")
-#     plt.xlabel("Value LR")
-#     plt.ylabel("Cum reward", rotation=90)
-# plt.show()
-# plt.clf()
-#
+# q-learning methods
+# ae, ae_plus, qt_opt, naf, picnn, wirefitting, sql, optimalq
+
+# Bimodal
+# for idx, a in enumerate(['ActorExpert', 'ActorExpert_Plus', 'SoftQlearning', 'NAF', 'PICNN', 'QT_OPT', 'WireFitting', 'OptimalQ']):
+# Pendulum
+
+for p in dicts:
+    for idx, a in enumerate(agents):
+
+        plt_xticks = dicts[p][a][1]
+        plt_x = dicts[p][a][2]
+        plt_y = dicts[p][a][3]
+        plt.plot(plt_xticks, plt_y, colors[idx], label="{}".format(a))
+        # plt.ylim(0, 1.5)
+
+        if show_label:
+            plt.xticks(plt_xticks, plt_x)
+            # plt.yticks(plt_yticks, plt_yticks)
+
+        else:
+            plt.xticks(plt_xticks, [])
+            # plt.yticks(plt_yticks, [])
+
+    if show_label:
+        plt.legend()
+        plt.title("{} sensitivity curve".format(p))
+        plt.xlabel(p)
+        plt.ylabel("0.5 AUC", rotation=90)
+
+    plt.savefig("combined_{}_sensitivity_curve.png".format(p))
+    # plt.show()
+    plt.clf()
+
 # # policy + value
 # for idx, a in enumerate(['ActorExpert_Separate', 'ActorExpert_Plus_Separate', 'SoftQlearning', 'ActorCritic_Separate', 'SoftActorCritic', 'DDPG']):
 #
@@ -196,5 +198,5 @@ for agent_filename in ['forward_kl', 'reverse_kl']:
 #     plt.xlabel("Policy LR")
 #     plt.ylabel("Cum reward", rotation=90)
 # plt.show()
-#
-# # ac_separate, sac, sql, ddpg, ae, ae_plus
+
+# ac_separate, sac, sql, ddpg, ae, ae_plus
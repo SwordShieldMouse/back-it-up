do unscaled reverse KL. Adjust sweep settings (fixed 128 layersize for RKL, keep same for FKL). Logging now also works for tf 1.8. Reduce evaluation during training

deep_experiments/agents/network/forwardkl_network.py
@@ -10,7 +10,7 @@ import quadpy
 import itertools
 from scipy.special import binom
 from .representations.separate_network import *
-
+from utils.main_utils import write_summary
 
 class ForwardKLNetwork(BaseNetwork):
     def __init__(self, config):
@@ -18,6 +18,9 @@ class ForwardKLNetwork(BaseNetwork):
 
         self.config = config
         self.optim_type = config.optim_type
+
+        self.writer = config.writer
+        self.writer_step = 0
 
         self.use_true_q = False
         if config.use_true_q == "True":
@@ -113,6 +116,16 @@ class ForwardKLNetwork(BaseNetwork):
         state_batch = torch.FloatTensor(state_batch).to(self.device)
         action, log_prob, z, mean, std = self.pi_net.evaluate(state_batch)
 
+        for dim in range(np.shape(action)[1]):
+            # for tf 1.8
+            write_summary(self.writer, self.writer_step, mean[0][dim], tag='mean/[{}]'.format(dim))
+            write_summary(self.writer, self.writer_step, std[0][dim], tag='std/[{}]'.format(dim))
+
+            # for tf 1.14 and above
+            # self.writer.add_scalar('mean/[{}]'.format(dim), mean[0][dim], self.writer_step)
+            # self.writer.add_scalar('std/[{}]'.format(dim), std[0][dim], self.writer_step)
+
+        self.writer_step += 1
         return action.detach().numpy()
 
     def predict_action(self, state_batch):

deep_experiments/agents/network/representations/separate_network.py
@@ -80,8 +80,9 @@ class PolicyNetwork(nn.Module):
         # log_std = torch.clamp(self.log_std_linear(x), self.log_std_min, self.log_std_max)
         # std = torch.exp(log_std)
 
-        std = torch.log(1 + torch.exp(self.log_std_linear(x)))
-        std = torch.clamp(std, np.exp(self.log_std_min), np.exp(self.log_std_max))
+        p = torch.clamp(self.log_std_linear(x), -19.99, 7.39)
+        std = torch.log(1 + torch.exp(p))
+        # std = torch.clamp(std, np.exp(self.log_std_min), np.exp(self.log_std_max))
 
         return mean, std
 
@@ -123,10 +124,16 @@ class PolicyNetwork(nn.Module):
         return stacked_log_prob
 
     def get_distribution(self, mean, std):
-        if self.action_dim == 1:
-            normal = Normal(mean, std)
-        else:
-            normal = MultivariateNormal(mean, torch.diag_embed(std))
+
+        try:
+            if self.action_dim == 1:
+                normal = Normal(mean, std)
+            else:
+                normal = MultivariateNormal(mean, torch.diag_embed(std))
+
+        except:
+            print("Error occured with mean {}, std {}".format(mean, std))
+            exit()
         return normal
 
     def atanh(self, x):

deep_experiments/agents/network/reversekl_network.py
@@ -10,6 +10,7 @@ import itertools
 from scipy.special import binom
 import environments
 from .representations.separate_network import *
+from utils.main_utils import write_summary
 
 
 class ReverseKLNetwork(BaseNetwork):
@@ -18,6 +19,9 @@ class ReverseKLNetwork(BaseNetwork):
 
         self.config = config
         self.optim_type = config.optim_type
+
+        self.writer = config.writer
+        self.writer_step = 0
 
         self.use_true_q = False
         if config.use_true_q == "True":
@@ -112,6 +116,17 @@ class ReverseKLNetwork(BaseNetwork):
         state_batch = torch.FloatTensor(state_batch).to(self.device)
         action, log_prob, z, mean, std = self.pi_net.evaluate(state_batch)
 
+        for dim in range(np.shape(action)[1]):
+            # for tf 1.8
+            write_summary(self.writer, self.writer_step, mean[0][dim], tag='mean/[{}]'.format(dim))
+            write_summary(self.writer, self.writer_step, std[0][dim], tag='std/[{}]'.format(dim))
+
+            # for tf 1.14 and above
+            # self.writer.add_scalar('mean/[{}]'.format(dim), mean[0][dim], self.writer_step)
+            # self.writer.add_scalar('std/[{}]'.format(dim), std[0][dim], self.writer_step)
+
+        self.writer_step += 1
+
         return action.detach().numpy()
 
     def predict_action(self, state_batch):
@@ -186,7 +201,7 @@ class ReverseKLNetwork(BaseNetwork):
 
                     intgrl_logprob = self.pi_net.get_logprob(state_batch, self.tiled_intgrl_actions)
 
-                    integrands = - torch.exp(intgrl_logprob.squeeze()) * ((intgrl_q_val.squeeze()).detach() - self.entropy_scale * intgrl_logprob.squeeze())
+                    integrands = - torch.exp(intgrl_logprob.squeeze()) * ((intgrl_q_val.squeeze()/self.entropy_scale).detach() - intgrl_logprob.squeeze())
 
                 policy_loss = (integrands * self.intgrl_weights.repeat(self.batch_size)).reshape(self.batch_size, -1).sum(-1).mean(-1)
 
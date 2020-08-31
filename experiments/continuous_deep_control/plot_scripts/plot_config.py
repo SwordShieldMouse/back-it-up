57bd90156ea22cc5ec4f46896e5d475289bcad52
Sungsu Lim <ss.sungsulim@gmail.com>
2020-08-31 17:07:35 -0600

    added plot_config.py

---
+++ experiments/continuous_deep_control/plot_scripts/plot_config.py
@@ -0,0 +1,28 @@
+
+
+def get_xyrange(envname):
+    xmax = None
+
+    if envname == 'ContinuousBandits':
+        ymin = [-0.5, -0.5]
+        ymax = [2.0, 2.0]
+
+    elif envname == 'Pendulum-v0':
+        ymin = [-1600, -1600]
+        ymax = [-100, -100]
+
+    elif envname == 'Reacher-v2':
+        ymin = [-80, -80]
+        ymax = [0, 0]
+
+    elif envname == "Swimmer-v2":
+        ymin = [0, 0]
+        ymax = [40, 40]
+
+    else:
+        raise ValueError("Invalid environment name")
+
+    if xmax is None:
+        return None, ymin, ymax
+    else:
+        return xmax, ymin, ymax

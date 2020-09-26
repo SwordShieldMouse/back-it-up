c8482e41afe3f997f551001e43425d4a3e9f46a1
Hugo <hsilva664>
2020-09-26 20:10:43 -0300

    small fix

---
+++ experiments/continuous_deep_control/slurm_scripts/run_script_gpu_template.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+ENV_NAME=$1
+AGENT_NAME=$2
+INDEX=$3
+
+# pip3 uninstall 
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/hugoluis/.mujoco/mjpro150/bin
+export CUDA_VISIBLE_DEVICES=`expr $3 % 2`
+python3 experiments/continuous_deep_control/nonlinear_run.py --env_json experiments/continuous_deep_control/jsonfiles/environment/$ENV_NAME.json --agent_json experiments/continuous_deep_control/jsonfiles/agent/$AGENT_NAME.json --index $3

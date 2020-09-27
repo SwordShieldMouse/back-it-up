bb324ecd4aac097449688a9f6dbf2e7627cdfc81
Hugo <hsilva664>
2020-09-26 23:44:11 -0300

    beluga

---
+++ experiments/continuous_deep_control/slurm_scripts/beluga/Reacher_forwardkl_venv.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --job-name=Reacher_fkl
+#SBATCH --output=/home/hugoluis/scratch/back-it-up/logs/Reacher/fkl/%A%a.out
+#SBATCH --error=/home/hugoluis/scratch/back-it-up/logs/Reacher/fkl/%A%a.err
+
+#SBATCH --array=0-2429:4
+
+#SBATCH --cpus-per-task=4
+#SBATCH --time=5:30:00
+#SBATCH --mem-per-cpu=6000M
+
+#SBATCH --account=def-whitem
+#SBATCH --gres=gpu:2
+
+ENV_NAME=Reacher-v2
+AGENT_NAME=forward_kl_big
+
+echo Running..$ENV_NAME $AGENT_NAME $SLURM_ARRAY_TASK_ID
+
+export OMP_NUM_THREADS=1
+export MKL_NUM_THREADS=1
+
+increment=1
+let "end_idx=$SLURM_ARRAY_TASK_ID+3"
+
+parallel --jobs 4 "source ~/sungsu_env/bin/activate; bash experiments/continuous_deep_control/slurm_scripts/run_script_gpu_template.sh $ENV_NAME $AGENT_NAME {}" ::: $(seq ${SLURM_ARRAY_TASK_ID} ${increment} ${end_idx})
+
+

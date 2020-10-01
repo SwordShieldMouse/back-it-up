28481a55601701cc2cac672951ca307ba0e941b8
Hugo <hsilva664>
2020-10-01 01:36:24 -0300

    beluga

---
+++ experiments/continuous_deep_control/slurm_scripts/beluga/Reacher_reversekl_venv_error.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --job-name=Reacher_rkl_ll_mul
+#SBATCH --output=/home/hugoluis/scratch/back-it-up/logs/Reacher/rkl/%A%a.out
+#SBATCH --error=/home/hugoluis/scratch/back-it-up/logs/Reacher/rkl/%A%a.err
+
+#SBATCH --array=1564,1565,1566,1567,2165,2171,2183,2189,2207,2225,2231,2261,2267,2309,2333,2345,2363,2411
+
+#SBATCH --cpus-per-task=1
+#SBATCH --time=16:30:00
+#SBATCH --mem-per-cpu=6000M
+
+#SBATCH --account=def-whitem
+
+#SBATCH --gres=gpu:1
+
+ENV_NAME=Reacher-v2
+AGENT_NAME=reverse_kl_ll_big
+
+echo Running..$ENV_NAME $AGENT_NAME $SLURM_ARRAY_TASK_ID
+
+export OMP_NUM_THREADS=1
+export MKL_NUM_THREADS=1
+
+source ~/sungsu_env/bin/activate; bash experiments/continuous_deep_control/slurm_scripts/run_script_gpu_template_error.sh $ENV_NAME $AGENT_NAME ${SLURM_ARRAY_TASK_ID}
+
+

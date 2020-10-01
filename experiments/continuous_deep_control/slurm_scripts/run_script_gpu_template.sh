222ae2d608dd74b1d3bee34643d7a4776f909926
Hugo <hsilva664>
2020-10-01 00:13:18 -0300

    graham and cedar

--- experiments/continuous_deep_control/slurm_scripts/run_script_gpu_template.sh
+++ experiments/continuous_deep_control/slurm_scripts/run_script_gpu_template.sh
@@ -6,4 +6,4 @@ INDEX=$3
 # pip3 uninstall 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/hugoluis/.mujoco/mjpro150/bin
 export CUDA_VISIBLE_DEVICES=`expr $3 % 2`
-stdbuf -o0 -e0 python3 experiments/continuous_deep_control/nonlinear_run.py --env_json experiments/continuous_deep_control/jsonfiles/environment/$ENV_NAME.json --agent_json experiments/continuous_deep_control/jsonfiles/agent/$AGENT_NAME.json --index $3
+unbuffer python3 experiments/continuous_deep_control/nonlinear_run.py --env_json experiments/continuous_deep_control/jsonfiles/environment/$ENV_NAME.json --agent_json experiments/continuous_deep_control/jsonfiles/agent/$AGENT_NAME.json --index $3

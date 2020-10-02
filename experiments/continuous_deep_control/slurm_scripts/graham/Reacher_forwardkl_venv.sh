3ede353ce95f0164678f72ab5881d39541b07719
Hugo <hsilva664>
2020-10-02 19:36:43 -0300

    fix

--- experiments/continuous_deep_control/slurm_scripts/graham/Reacher_forwardkl_venv.sh
+++ experiments/continuous_deep_control/slurm_scripts/graham/Reacher_forwardkl_venv.sh
@@ -3,14 +3,14 @@
 #SBATCH --output=/home/hugoluis/scratch/back-it-up/logs/Reacher/fkl/%A%a.out
 #SBATCH --error=/home/hugoluis/scratch/back-it-up/logs/Reacher/fkl/%A%a.err
 
-#SBATCH --array=0-1620:4
+#SBATCH --array=0-1620
 
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=1
 #SBATCH --time=16:30:00
 #SBATCH --mem-per-cpu=6000M
 
 #SBATCH --account=def-whitem
-#SBATCH --gres=gpu:4
+#SBATCH --gres=gpu:1
 
 ENV_NAME=Reacher-v2
 AGENT_NAME=forward_kl_big
@@ -20,9 +20,6 @@ echo Running..$ENV_NAME $AGENT_NAME $SLURM_ARRAY_TASK_ID
 export OMP_NUM_THREADS=1
 export MKL_NUM_THREADS=1
 
-increment=1
-let "end_idx=$SLURM_ARRAY_TASK_ID+3"
-
-parallel --jobs 4 "source ~/sungsu_env/bin/activate; bash experiments/continuous_deep_control/slurm_scripts/run_script_gpu_template.sh $ENV_NAME $AGENT_NAME {}" ::: $(seq ${SLURM_ARRAY_TASK_ID} ${increment} ${end_idx})
+source ~/sungsu_env/bin/activate; bash experiments/continuous_deep_control/slurm_scripts/run_script_gpu_template_error.sh $ENV_NAME $AGENT_NAME ${SLURM_ARRAY_TASK_ID}
 
 

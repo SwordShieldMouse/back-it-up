b4c149fee98ca8f629cbcb07d97ded17a4543e90
Hugo <hsilva664>
2020-10-06 00:34:33 -0300

    graham

--- experiments/continuous_deep_control/slurm_scripts/graham/Pendulum/Pendulum_reversekl_venv.sh
+++ experiments/continuous_deep_control/slurm_scripts/graham/Pendulum/Pendulum_reversekl_venv.sh
@@ -3,14 +3,14 @@
 #SBATCH --output=/home/hugoluis/scratch/back-it-up/logs/Pendulum/rkl/%A%a.out
 #SBATCH --error=/home/hugoluis/scratch/back-it-up/logs/Pendulum/rkl/%A%a.err
 
-#SBATCH --array=0-2429%200
+#SBATCH --array=0-2429:3%200
 
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=3
 #SBATCH --time=8:30:00
 #SBATCH --mem-per-cpu=6000M
 
 #SBATCH --account=def-whitem
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:3
 
 ENV_NAME=Pendulum-v0
 AGENT_NAME=reverse_kl_rpm_big
@@ -20,6 +20,9 @@ echo Running..$ENV_NAME $AGENT_NAME $SLURM_ARRAY_TASK_ID
 export OMP_NUM_THREADS=1
 export MKL_NUM_THREADS=1
 
-source ~/sungsu_env/bin/activate; bash experiments/continuous_deep_control/slurm_scripts/run_script_gpu_template_error.sh $ENV_NAME $AGENT_NAME ${SLURM_ARRAY_TASK_ID}
+increment=1
+let "end_idx=$SLURM_ARRAY_TASK_ID+2"
+
+parallel --jobs 3 "source ~/sungsu_env/bin/activate; bash experiments/continuous_deep_control/slurm_scripts/run_script_gpu_template.sh $ENV_NAME $AGENT_NAME {}" ::: $(seq ${SLURM_ARRAY_TASK_ID} ${increment} ${end_idx})
 
 

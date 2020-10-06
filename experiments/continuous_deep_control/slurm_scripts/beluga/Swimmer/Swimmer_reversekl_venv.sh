2887002f5e50d64ffda6bb43ee32947673352a5b
Hugo <hsilva664>
2020-10-06 00:22:03 -0300

    .

--- experiments/continuous_deep_control/slurm_scripts/beluga/Swimmer/Swimmer_reversekl_venv.sh
+++ experiments/continuous_deep_control/slurm_scripts/beluga/Swimmer/Swimmer_reversekl_venv.sh
@@ -3,15 +3,15 @@
 #SBATCH --output=/home/hugoluis/scratch/back-it-up/logs/Swimmer/rkl/%A%a.out
 #SBATCH --error=/home/hugoluis/scratch/back-it-up/logs/Swimmer/rkl/%A%a.err
 
-#SBATCH --array=0-2429%200
+#SBATCH --array=0-2429:3%200
 
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=3
 #SBATCH --time=8:00:00
 #SBATCH --mem-per-cpu=6000M
 
 #SBATCH --account=def-whitem
 
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:3
 
 ENV_NAME=Swimmer-v2
 AGENT_NAME=reverse_kl_ll_big
@@ -21,6 +21,9 @@ echo Running..$ENV_NAME $AGENT_NAME $SLURM_ARRAY_TASK_ID
 export OMP_NUM_THREADS=1
 export MKL_NUM_THREADS=1
 
-source ~/sungsu_env/bin/activate; bash experiments/continuous_deep_control/slurm_scripts/run_script_gpu_template_error.sh $ENV_NAME $AGENT_NAME ${SLURM_ARRAY_TASK_ID}
+increment=1
+let "end_idx=$SLURM_ARRAY_TASK_ID+2"
+
+parallel --jobs 3 "source ~/sungsu_env/bin/activate; bash experiments/continuous_deep_control/slurm_scripts/run_script_gpu_template.sh $ENV_NAME $AGENT_NAME {}" ::: $(seq ${SLURM_ARRAY_TASK_ID} ${increment} ${end_idx})
 
 
---
+++ experiments/continuous_deep_control/slurm_scripts/graham/Swimmer/Reacher_forwardkl_venv_error.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --job-name=Reacher_fkl
+#SBATCH --output=/home/hugoluis/scratch/back-it-up/logs/Reacher/fkl/%A%a.out
+#SBATCH --error=/home/hugoluis/scratch/back-it-up/logs/Reacher/fkl/%A%a.err
+
+#SBATCH --array=2165,2171,2177,2183,2189,2195,2201,2207,2213,2219,2225,2231,2237,2243,2249,2255,2261,2267,2273,2279,2285,2291,2297,2303,2309,2315,2321,2327,2333,2339,2345,2351,2357,2363,2369,2375,2381,2387,2393,2399,2405,2411,2417,2423,2429
+
+#SBATCH --cpus-per-task=1
+#SBATCH --time=16:30:00
+#SBATCH --mem-per-cpu=6000M
+
+#SBATCH --account=def-whitem
+#SBATCH --gres=gpu:1
+
+ENV_NAME=Reacher-v2
+AGENT_NAME=forward_kl_big
+
+echo Running..$ENV_NAME $AGENT_NAME $SLURM_ARRAY_TASK_ID
+
+export OMP_NUM_THREADS=1
+export MKL_NUM_THREADS=1
+
+source ~/sungsu_env/bin/activate; bash experiments/continuous_deep_control/slurm_scripts/run_script_gpu_template_error.sh $ENV_NAME $AGENT_NAME ${SLURM_ARRAY_TASK_ID}
+
+

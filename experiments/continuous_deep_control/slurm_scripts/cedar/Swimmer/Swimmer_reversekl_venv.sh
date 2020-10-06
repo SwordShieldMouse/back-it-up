6cacf39b46ccbdfbd3b61c4f590e05f617dcf99f
Hugo <hsilva664>
2020-10-05 23:27:54 -0300

    new runs

---
+++ experiments/continuous_deep_control/slurm_scripts/cedar/Swimmer/Swimmer_reversekl_venv.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --job-name=Swimmer_rkl_rpm_mul
+#SBATCH --output=/home/hugoluis/scratch/back-it-up/logs/Swimmer/rkl/%A%a.out
+#SBATCH --error=/home/hugoluis/scratch/back-it-up/logs/Swimmer/rkl/%A%a.err
+
+#SBATCH --array=0-2429%200
+
+#SBATCH --cpus-per-task=1
+#SBATCH --time=6:30:00
+#SBATCH --mem-per-cpu=6000M
+
+#SBATCH --account=rrg-whitem
+
+#SBATCH --gres=gpu:v100l:1
+
+ENV_NAME=Swimmer-v2
+AGENT_NAME=reverse_kl_rpm_big
+
+echo Running..$ENV_NAME $AGENT_NAME $SLURM_ARRAY_TASK_ID
+
+export OMP_NUM_THREADS=1
+export MKL_NUM_THREADS=1
+
+source ~/sungsu_env/bin/activate; bash experiments/continuous_deep_control/slurm_scripts/run_script_gpu_template_error.sh $ENV_NAME $AGENT_NAME ${SLURM_ARRAY_TASK_ID}
+
+

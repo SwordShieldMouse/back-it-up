52f5389ea1b1db9b17a10db2658f3031e5d48dce
Hugo <hsilva664>
2020-10-14 22:58:35 -0300

    a

---
+++ experiments/continuous_deep_control/slurm_scripts/graham/Swimmer/Swimmer_reversekl_ll_venv.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --job-name=Swimmer_ll
+#SBATCH --output=./logs/Swimmer/rkl/%A%a.out
+#SBATCH --error=./logs/Swimmer/rkl/%A%a.err
+
+#SBATCH --array=0-1599:4
+
+#SBATCH --cpus-per-task=4
+#SBATCH --time=20:00:00
+#SBATCH --mem-per-cpu=7000M
+
+#SBATCH --account=def-whitem
+
+ENV_NAME=Swimmer-v2
+AGENT_NAME=reverse_kl_ll_big
+
+echo Running..$ENV_NAME $AGENT_NAME $SLURM_ARRAY_TASK_ID
+
+export OMP_NUM_THREADS=1
+export MKL_NUM_THREADS=1
+
+increment=1
+let "end_idx=$SLURM_ARRAY_TASK_ID+3"
+
+parallel --jobs 4 "source ~/sungsu_env/bin/activate; bash experiments/continuous_deep_control/slurm_scripts/run_script_template.sh $ENV_NAME $AGENT_NAME {}" ::: $(seq ${SLURM_ARRAY_TASK_ID} ${increment} ${end_idx})
+
+

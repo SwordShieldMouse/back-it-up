5de959e3ffe6e665c2a75a69d8a23fb84eb2d772
Hugo <hsilva664>
2020-11-18 16:52:42 -0300

    cedar gpu

--- experiments/continuous_deep_control/slurm_scripts/cedar/Swimmer/Swimmer_forwardkl_venv_err.sh
+++ experiments/continuous_deep_control/slurm_scripts/cedar/Swimmer/Swimmer_forwardkl_venv_err.sh
@@ -3,13 +3,15 @@
 #SBATCH --output=./logs/Swimmer/fkl/%A%a.out
 #SBATCH --error=./logs/Swimmer/fkl/%A%a.err
 
-#SBATCH --array=0-2283:4
+#SBATCH --array=0-2283
 
-#SBATCH --cpus-per-task=4
-#SBATCH --time=10:00:00
+#SBATCH --cpus-per-task=1
+#SBATCH --time=8:00:00
 #SBATCH --mem-per-cpu=8000M
 
 #SBATCH --account=rrg-whitem
+
+#SBATCH --gres=gpu:v100l:1
 
 ENV_NAME=Swimmer-v2
 AGENT_NAME=forward_kl_big
@@ -21,15 +23,16 @@ echo Running..$ENV_NAME $AGENT_NAME $SLURM_ARRAY_TASK_ID
 export OMP_NUM_THREADS=1
 export MKL_NUM_THREADS=1
 
-THESE_ERROR_RUNS=()
+# THESE_ERROR_RUNS=()
 
-let "end_idx=$SLURM_ARRAY_TASK_ID+3"
-for ((i=$SLURM_ARRAY_TASK_ID; i<= $end_idx; i++)); do
-    THESE_ERROR_RUNS+=( ${ERROR_RUNS[$i]} )
-done
+# let "end_idx=$SLURM_ARRAY_TASK_ID+3"
+# for ((i=$SLURM_ARRAY_TASK_ID; i<= $end_idx; i++)); do
+#     THESE_ERROR_RUNS+=( ${ERROR_RUNS[$i]} )
+# done
 
-increment=1
+# increment=1
 
-parallel --jobs 4 "source ~/sungsu_env/bin/activate; bash experiments/continuous_deep_control/slurm_scripts/run_script_template.sh $ENV_NAME $AGENT_NAME {}" ::: ${THESE_ERROR_RUNS[@]}
+# parallel --jobs 4 "source ~/sungsu_env/bin/activate; bash experiments/continuous_deep_control/slurm_scripts/run_script_template.sh $ENV_NAME $AGENT_NAME {}" ::: ${THESE_ERROR_RUNS[@]}
 
-
+source ~/sungsu_env/bin/activate;
+bash experiments/continuous_deep_control/slurm_scripts/run_script_template.sh $ENV_NAME $AGENT_NAME ${ERROR_RUNS[$SLURM_ARRAY_TASK_ID]}

6fec2a9259bfe1c8a120095a091eb3b189f5b566
Hugo <hsilva664>
2021-04-15 22:01:27 -0300

    remove tf writer

--- experiments/continuous_deep_control/nonlinear_run.py
+++ experiments/continuous_deep_control/nonlinear_run.py
@@ -132,9 +132,10 @@ def main():
     START_DATETIME = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
     log_dir = './{}/{}results/log_summary/{}/{}_{}_{}'.format(args.out_dir, str(env_json['environment']), str(agent_json['agent']), str(SETTING_NUM), str(RUN_NUM), str(START_DATETIME))
 
-    writer = tf.summary.FileWriter(log_dir)
+    # writer = tf.summary.FileWriter(log_dir)
     #writer = tf.compat.v1.summary.FileWriter(log_dir)
     # writer = SummaryWriter(log_dir)
+    writer = None
     agent_params["writer"] = writer
 
     # init config and merge custom config settings from json

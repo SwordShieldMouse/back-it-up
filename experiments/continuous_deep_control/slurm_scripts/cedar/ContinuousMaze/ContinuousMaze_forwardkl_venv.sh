e0ed2d26cb584c7430539839b7fd5335f0d47a92
Hugo <hsilva664>
2021-04-22 00:25:29 -0300

    Medium map

--- experiments/continuous_deep_control/slurm_scripts/cedar/ContinuousMaze/ContinuousMaze_forwardkl_venv.sh
+++ experiments/continuous_deep_control/slurm_scripts/cedar/ContinuousMaze/ContinuousMaze_forwardkl_venv.sh
@@ -1,30 +1,29 @@
 #!/bin/bash
 #SBATCH --job-name=ContinuousMaze_fkl
-#SBATCH --output=./logs/ContinuousMaze/fkl/%A%a.out
-#SBATCH --error=./logs/ContinuousMaze/fkl/%A%a.err
+#SBATCH --output=./logs/ContinuousMaze_fkl_%A%a.out
+#SBATCH --error=./logs/ContinuousMaze_fkl_%A%a.err
 
-#SBATCH --array=0-287:4
+#SBATCH --array=0-59:4
 
 #SBATCH --cpus-per-task=4
-#SBATCH --time=4:00:00
-#SBATCH --mem-per-cpu=3000M
+#SBATCH --time=6:00:00
+#SBATCH --mem-per-cpu=4G
 
 #SBATCH --account=rrg-whitem
 
-module restore old_stdenv
-
 ENV_NAME=ContinuousMaze
-AGENT_NAME=forward_kl_maze
+AGENT_NAME=forward_kl_medium_maze
 
 echo Running..$ENV_NAME $AGENT_NAME $SLURM_ARRAY_TASK_ID
 
 export OMP_NUM_THREADS=1
 export MKL_NUM_THREADS=1
 
+module load singularity
 
 increment=1
 let "end_idx=$SLURM_ARRAY_TASK_ID+3"
 
-parallel --jobs 4 "source ~/sungsu_env/bin/activate; bash experiments/continuous_deep_control/slurm_scripts/run_script_maze.sh $ENV_NAME $AGENT_NAME {}" ::: $(seq ${SLURM_ARRAY_TASK_ID} ${increment} ${end_idx})
+parallel --jobs 4 "singularity exec --cleanenv --no-home --writable --bind .:/scratch --pwd /scratch ~/singularity_environment/sungsu_test_sandbox bash experiments/continuous_deep_control/slurm_scripts/run_script_maze_singularity.sh $ENV_NAME $AGENT_NAME {}"  ::: $(seq ${SLURM_ARRAY_TASK_ID} ${increment} ${end_idx})
 
 

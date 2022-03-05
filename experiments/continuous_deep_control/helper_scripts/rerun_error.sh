aba706736b27986382b030d8a0eeb54496c465fa
hugosilva664 <metallic-silver@hotmail.com>
2022-03-05 00:07:49 -0300

    automatic sbatch rescheduling

--- experiments/continuous_deep_control/helper_scripts/rerun_error.sh
+++ experiments/continuous_deep_control/helper_scripts/rerun_error.sh
@@ -5,12 +5,13 @@
 #SBATCH --error=./slurm_logs/rerun_error.err
 #SBATCH --array=1-1
 #SBATCH --time=30:00
+#SBATCH --mem_per_cpu=2G
 #SBATCH --account=def-whitem
 #SBATCH --dependency=singleton
 
 # TODO: REMOVE THIS
 rm -rf results
 . $HOME/my_cc_scripts/custom_commands.sh
-CC_MY_SINGULARITY_EXEC python -m helper_scripts.debug_find_error_runs > $HOME/my_cc_scripts/sbatch_files/${CLUSTER_NAME}.txt
+CC_MY_SINGULARITY_EXEC pipenv run python -m helper_scripts.debug_find_error_runs > $HOME/my_cc_scripts/sbatch_files/${CLUSTER_NAME}.txt
 CC_SBATCH_FROM_FILE --time 4:00:00 --mem_per_cpu 8G
 

86e8e6ae5a907138bd97eaacad74de754de4b6db
hugosilva664 <metallic-silver@hotmail.com>
2022-03-08 00:02:25 -0300

    new sweeps for gmm

--- experiments/continuous_deep_control/helper_scripts/generate_slurm_script.py
+++ experiments/continuous_deep_control/helper_scripts/generate_slurm_script.py
@@ -1,12 +1,15 @@
 import os
 
-for i in range(125 * 5):
-    os.system("echo 'pipenv run python nonlinear_run.py --env_json jsonfiles/environment/EasyContinuousWorld.json --agent_json jsonfiles/agent/world/fkl.json --index {}' >> cedar.txt".format(i))
-    os.system("echo 'pipenv run python nonlinear_run.py --env_json jsonfiles/environment/EasyContinuousWorld.json --agent_json jsonfiles/agent/world/rkl.json --index {}' >> cedar.txt".format(i))
+# for i in range(125 * 5):
+#     os.system("echo 'pipenv run python nonlinear_run.py --env_json jsonfiles/environment/EasyContinuousWorld.json --agent_json jsonfiles/agent/world/fkl.json --index {}' >> commands.txt".format(i))
+#     os.system("echo 'pipenv run python nonlinear_run.py --env_json jsonfiles/environment/EasyContinuousWorld.json --agent_json jsonfiles/agent/world/rkl.json --index {}' >> commands.txt".format(i))
+#
+#     os.system("echo 'pipenv run python nonlinear_run.py --env_json jsonfiles/environment/MultimodalContinuousWorld.json --agent_json jsonfiles/agent/world/fkl.json --index {}' >> commands.txt".format(i))
+#     os.system("echo 'pipenv run python nonlinear_run.py --env_json jsonfiles/environment/MultimodalContinuousWorld.json --agent_json jsonfiles/agent/world/rkl.json --index {}' >> commands.txt".format(i))
 
-    os.system("echo 'pipenv run python nonlinear_run.py --env_json jsonfiles/environment/MultimodalContinuousWorld.json --agent_json jsonfiles/agent/world/fkl.json --index {}' >> gr_nv.txt".format(i))
-    os.system("echo 'pipenv run python nonlinear_run.py --env_json jsonfiles/environment/MultimodalContinuousWorld.json --agent_json jsonfiles/agent/world/rkl.json --index {}' >> gr_nv.txt".format(i))
+for i in range(375 * 5):
+    os.system("echo 'pipenv run python nonlinear_run.py --env_json jsonfiles/environment/EasyContinuousWorld.json --agent_json jsonfiles/agent/world/fkl_gmm.json --index {}' >> commands.txt".format(i))
+    os.system("echo 'pipenv run python nonlinear_run.py --env_json jsonfiles/environment/EasyContinuousWorld.json --agent_json jsonfiles/agent/world/rkl_gmm.json --index {}' >> commands.txt".format(i))
 
-# for i in range(8 * 2):
-#     os.system("echo 'pipenv run python nonlinear_run.py --env_json jsonfiles/environment/EasyContinuousWorld.json --agent_json jsonfiles/agent/debug/fkl.json --index {}' >> cedar.txt".format(i))
-#     os.system("echo 'pipenv run python nonlinear_run.py --env_json jsonfiles/environment/EasyContinuousWorld.json --agent_json jsonfiles/agent/debug/rkl.json --index {}' >> cedar.txt".format(i))
+    os.system("echo 'pipenv run python nonlinear_run.py --env_json jsonfiles/environment/MultimodalContinuousWorld.json --agent_json jsonfiles/agent/world/fkl_gmm.json --index {}' >> commands.txt".format(i))
+    os.system("echo 'pipenv run python nonlinear_run.py --env_json jsonfiles/environment/MultimodalContinuousWorld.json --agent_json jsonfiles/agent/world/rkl_gmm.json --index {}' >> commands.txt".format(i))
